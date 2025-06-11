b6e39d4f36948e48db7025eaf3d5bc44276fd17f
Matty Maclean <macleanmatty@gmail.com>
2025-06-11 13:37:16 +0100

    Use JSONENCODE to store list of domains

--- terraform/domains.tf
+++ terraform/domains.tf
@@ -1,11 +1,11 @@
 # Maintain list of domains in state
 resource "terraform_data" "domains" {
-  input = var.domain
+  input = jsonencode(concat(try(jsondecode(terraform_data.domains.output), []), [var.domain]))
 }
 
 locals {
   # Get existing domains from state, or start with empty list
-  existing_domains = try(tolist(terraform_data.domains.output), [])
+  existing_domains = try(jsondecode(terraform_data.domains.output), [])
   # Add new domain if it's not already in the list
   domains = toset(concat(local.existing_domains, [var.domain]))
 }
