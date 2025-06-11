40ad930991b2b98f55c5e84da0fa3655446d9c99
Matty Maclean <macleanmatty@gmail.com>
2025-06-11 17:03:28 +0100

    Revert "Use JSONENCODE to store list of domains"
    This reverts commit b6e39d4f36948e48db7025eaf3d5bc44276fd17f.

--- terraform/domains.tf
+++ terraform/domains.tf
@@ -1,11 +1,11 @@
 # Maintain list of domains in state
 resource "terraform_data" "domains" {
-  input = jsonencode(concat(try(jsondecode(terraform_data.domains.output), []), [var.domain]))
+  input = var.domain
 }
 
 locals {
   # Get existing domains from state, or start with empty list
-  existing_domains = try(jsondecode(terraform_data.domains.output), [])
+  existing_domains = try(tolist(terraform_data.domains.output), [])
   # Add new domain if it's not already in the list
   domains = toset(concat(local.existing_domains, [var.domain]))
 }
