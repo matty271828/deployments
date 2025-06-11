da8a1b038ecc061a79c9dbf601254f6217415469
Matty Maclean <macleanmatty@gmail.com>
2025-06-11 11:11:20 +0100

    Remove load balancer configuration

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -1,40 +1,4 @@
-# Cloudflare Load Balancer - Shared across all domains
-resource "cloudflare_load_balancer" "main" {
-  zone_id        = cloudflare_zone.domains[var.domain].id  # Using the first domain as the primary zone
-  name           = "main-lb"
-  fallback_pool_id = cloudflare_load_balancer_pool.main.id
-  default_pool_ids = [cloudflare_load_balancer_pool.main.id]
-  proxied        = true
-}
-
-# Load Balancer Pool - Shared across all domains
-resource "cloudflare_load_balancer_pool" "main" {
-  account_id = var.cloudflare_account_id
-  name       = "main-pool"
-  origins {
-    name    = "origin-1"
-    address = digitalocean_droplet.multi-project-server[0].ipv4_address
-    enabled = true
-  }
-  monitor = cloudflare_load_balancer_monitor.main.id
-}
-
-# Load Balancer Monitor - Shared across all domains
-resource "cloudflare_load_balancer_monitor" "main" {
-  account_id     = var.cloudflare_account_id
-  description    = "main-monitor"
-  type           = "http"
-  interval       = 30
-  timeout        = 5
-  retries        = 3
-  method         = "GET"
-  path           = "/"
-  expected_body  = ""
-  expected_codes = "200"
-}
-
 # Cloudflare Zones - One for each domain
-# Using for_each to create a zone for each domain without overwriting existing ones
 resource "cloudflare_zone" "domains" {
   for_each = toset([var.domain])  # This will add new domains without affecting existing ones
   account_id = var.cloudflare_account_id
@@ -48,7 +12,7 @@ resource "cloudflare_record" "root" {
   name    = "@"
   content = digitalocean_droplet.multi-project-server[0].ipv4_address
   type    = "A"
-  proxied = true
+  proxied = true  # This enables Cloudflare's proxy (orange cloud)
 }
 
 # CNAME Records for www subdomains - One for each domain
@@ -58,5 +22,5 @@ resource "cloudflare_record" "www" {
   name    = "www"
   content = each.value.zone
   type    = "CNAME"
-  proxied = true
+  proxied = true  # This enables Cloudflare's proxy (orange cloud)
 } 
