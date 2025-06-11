6a18eed3242416a98a3824ab68464d26020ee75f
Matty Maclean <macleanmatty@gmail.com>
2025-06-11 17:15:20 +0100

    Add outputs to indicate what domains exist already

--- terraform/domains.tf
+++ terraform/domains.tf
@@ -13,4 +13,20 @@ locals {
 # Output the list of domains for future reference
 output "domains" {
   value = local.domains
+}
+
+# Additional outputs for better visibility in GitHub Actions
+output "current_domain" {
+  value = var.domain
+  description = "The domain being added in this run"
+}
+
+output "existing_domains" {
+  value = local.existing_domains
+  description = "List of domains that were already in state"
+}
+
+output "all_domains" {
+  value = local.domains
+  description = "Complete list of all domains after this run"
 } 
