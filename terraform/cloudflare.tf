f024c4440bab5313746e67028bde6e32cbcdf1b6
Matty Maclean <macleanmatty@gmail.com>
2025-06-11 12:00:42 +0100

    Fix terraform zone management

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -1,39 +1,23 @@
-# Get existing domains from state
-data "terraform_remote_state" "existing" {
-  backend = "local"
-  config = {
-    path = "${path.module}/terraform.tfstate"
-  }
-}
-
-locals {
-  existing_domains = try(toset(data.terraform_remote_state.existing.outputs.domains), toset([]))
-  all_domains = toset(concat([var.domain], tolist(local.existing_domains)))
-}
-
-# Cloudflare Zones - One for each domain
-resource "cloudflare_zone" "domains" {
-  for_each = local.all_domains
+# Cloudflare Zone
+resource "cloudflare_zone" "domain" {
   account_id = var.cloudflare_account_id
-  zone       = each.value
+  zone       = var.domain
 }
 
-# A Records for root domains - One for each domain
+# A Record for root domain
 resource "cloudflare_record" "root" {
-  for_each = cloudflare_zone.domains
-  zone_id = each.value.id
+  zone_id = cloudflare_zone.domain.id
   name    = "@"
   content = digitalocean_droplet.multi-project-server[0].ipv4_address
   type    = "A"
   proxied = true  # This enables Cloudflare's proxy (orange cloud)
 }
 
-# CNAME Records for www subdomains - One for each domain
+# CNAME Record for www subdomain
 resource "cloudflare_record" "www" {
-  for_each = cloudflare_zone.domains
-  zone_id = each.value.id
+  zone_id = cloudflare_zone.domain.id
   name    = "www"
-  content = each.value.zone
+  content = var.domain
   type    = "CNAME"
   proxied = true  # This enables Cloudflare's proxy (orange cloud)
 }
