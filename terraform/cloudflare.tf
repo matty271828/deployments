dbf7cfebe1cc2e265858caf1b2f74a60f9d6071f
Matty Maclean <macleanmatty@gmail.com>
2025-06-12 23:20:04 +0100

    Simplify retrieval of existing zones

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -1,36 +1,17 @@
 # Get existing zones for domains
-data "cloudflare_zones" "domains" {
-  filter {
-    name = join("|", local.domains)
-  }
+data "cloudflare_zone" "domains" {
+  for_each = toset(local.domains)
+  name     = each.value
 }
 
 locals {
   # Create a map of domain to zone_id for existing zones
-  existing_zones = { for zone in data.cloudflare_zones.domains.zones : zone.name => zone.id }
-  
-  # Filter out domains that already have zones
-  new_domains = [for domain in local.domains : domain if !contains(keys(local.existing_zones), domain)]
-}
-
-# Create zones only for new domains
-resource "cloudflare_zone" "domains" {
-  for_each = toset(local.new_domains)
-  account_id = var.cloudflare_account_id
-  zone       = each.value
-}
-
-# Combine existing and new zones
-locals {
-  all_zones = merge(
-    { for domain, id in local.existing_zones : domain => { id = id, zone = domain } },
-    { for domain, zone in cloudflare_zone.domains : domain => { id = zone.id, zone = domain } }
-  )
+  existing_zones = { for domain, zone in data.cloudflare_zone.domains : domain => { id = zone.id, zone = domain } }
 }
 
 # A Records for domains
 resource "cloudflare_record" "root" {
-  for_each = local.all_zones
+  for_each = local.existing_zones
   zone_id = each.value.id
   name    = "@"
   content = digitalocean_droplet.multi-project-server[0].ipv4_address
@@ -41,7 +22,7 @@ resource "cloudflare_record" "root" {
 
 # CNAME Records for domains
 resource "cloudflare_record" "www" {
-  for_each = local.all_zones
+  for_each = local.existing_zones
   zone_id = each.value.id
   name    = "www"
   content = each.value.zone
