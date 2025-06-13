9a5c3405004904551c80f8923dcba109c82af524
Matty Maclean <macleanmatty@gmail.com>
2025-06-13 19:03:37 +0100

    Only create single cloudflare pages project

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -1,16 +1,7 @@
-# Cloudflare Zone for each domain
-resource "cloudflare_zone" "domains" {
-  for_each = local.domain_map
-  
-  zone = each.value.domain
-  account_id = var.cloudflare_account_id
-}
-
 # Create Cloudflare Pages projects
 resource "cloudflare_pages_project" "frontend" {
-  for_each = local.domain_map
   account_id = var.cloudflare_account_id
-  name       = each.value.frontend_repo
+  name       = "leetrepeat"
   production_branch = "main"
   
   build_config {
@@ -22,18 +13,8 @@ resource "cloudflare_pages_project" "frontend" {
     type = "github"
     config {
       owner = "matty271828"  # Your GitHub username/organization
-      repo_name = each.value.frontend_repo
+      repo_name = "leetrepeat"
       production_branch = "main"
     }
   }
-}
-
-# Create custom domains for Pages projects
-resource "cloudflare_pages_domain" "custom_domain" {
-  for_each = local.domain_map
-  account_id = var.cloudflare_account_id
-  project_name = each.value.frontend_repo
-  domain = each.key
-
-  depends_on = [cloudflare_pages_project.frontend]
 }
