3aea04c69bf6ddab218fbe30df9dbd1284ab0491
Matty Maclean <macleanmatty@gmail.com>
2025-06-26 07:31:25 +0100

    Fix retrieving BREVO_API_KEY

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,13 +29,3 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
-
-variable "gmail_address" {
-  description = "Gmail address to forward emails to"
-  type        = string
-  
-  validation {
-    condition     = can(regex("^[a-zA-Z0-9._%+-]+@gmail\\.com$", var.gmail_address))
-    error_message = "Gmail address must be a valid Gmail address ending with @gmail.com"
-  }
-}
